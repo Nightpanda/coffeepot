a5ad49f5c7abd51bf0508519702cbb9caf15f8dc
Pekka Tiilikainen <tiilikap@gmail.com>
2018-02-25 13:26:03 +0200

    Adds externs to the compiler
    This allows us to tell the production compiler not to minify
    firebase external libraries function and variables.
    These are specified in the externs/firebase.js file.
    
    So for future reference: If you want to interop external
    javascript libraries in clojurescript, you need to use
    them this way.

---
+++ externs/firebase.js
@@ -0,0 +1,7 @@
+var firebase = {};
+firebase.initializeApp = function(){};
+firebase.auth = function(){};
+firebase.auth.GoogleAuthProvider = function(){};
+firebase.auth.signInWithPopup = function(){};
+firebase.auth.signOut = function(){};
+firebase.auth.onAuthStateChanged = function(){};

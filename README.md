5bd7a2acf4848d5d3df4c41d25a5304e8e4a24b7
Tiilikainen Pekka <pekka.tiilikainen@digia.com>
2018-07-27 15:49:44 +0300

    Adds instructions for starting development with cypress

--- README.md
+++ README.md
@@ -7,6 +7,18 @@ $ npm install -g firebase-tools
 $ firebase login
 $ firebase deploy
 
+## Development
+
+$ lein npm install
+$ lein figwheel
+
+### Local hosting to test production like environment
+$ firebase serve
+
+### Cypress testing
+$ lein npm install
+$ lein npm run cypress
+
 ## Deployment
 Builds cljs and deploys with firebase, asks for firebase login first if you are not logged in.
 $ build_deploy.sh
--- cypress/integration/user_login.js
+++ cypress/integration/user_login.js
@@ -3,8 +3,9 @@ import config from '../fixtures/config.js'
 describe('User', () => {
   it('logins with email', () => {
     cy.visit(config.devHostAddress)
-//Setup the test data
-//Firebase login is required at CLI
+    //Setup the test data
+    //Firebase login is required at CLI
+   //database is determined by firebase login
     cy.exec('firebase database:set /users cypress/fixtures/coffeepot.json --confirm')
     cy.get('#sign-in').click()
     const googleLogin = cy.get('#google-sign-in')
